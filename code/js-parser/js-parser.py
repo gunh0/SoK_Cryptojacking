9e5e12f142cd51bdfa6f1a4a760a405336feb214
devgunho <devgun@kakao.com>
2021-05-27 01:35:54 +0900

    Add js-parser

---
+++ code/js-parser/js-parser.py
@@ -0,0 +1,14 @@
+import os
+from bs4 import BeautifulSoup
+
+path = os.path.dirname(os.path.abspath(__file__))
+# print(path)
+path += '/test.html'
+# print(path)
+
+with open(path, encoding='UTF8') as fp:
+    soup = BeautifulSoup(fp)
+
+scripts = soup.find_all('script')
+for i in range(0, len(scripts)):
+    print("URL", scripts[i])
